43b8bf88b812dac6bd3b81b08db840b3462b0bb7
selpo <planet_selpo@yahoo.co.jp>
2019-09-28 20:30:04 +0900

    improved CMakeLists.txt

--- cmake/FindMPFR.cmake
+++ cmake/FindMPFR.cmake
@@ -1,29 +1,22 @@
 include(cmake/utils.cmake)  # for my_find_include, my_find_lib
 
 if(MSVC)
-	set(MPFR_ROOT_DEBUG "${MPFR_ROOT}/Debug")
-	set(MPFR_ROOT_RELEASE "${MPFR_ROOT}/Release")
+	my_find_include(MPFR_INCLUDE_DIR mpfr.h ${MPFR_ROOT}/Debug)
+	my_find_lib(MPFR_LIBRARY_DEBUG mpfr ${MPFR_ROOT}/Debug)
+	my_find_lib(MPFR_LIBRARY_RELEASE mpfr ${MPFR_ROOT}/Release)
+	set(MPFR_LIBRARY debug ${MPFR_LIBRARY_DEBUG} optimized ${MPFR_LIBRARY_RELEASE})
+	mark_as_advanced(MPFR_LIBRARY_DEBUG MPFR_LIBRARY_RELEASE)
 else()
-	set(MPFR_ROOT_DEBUG "${MPFR_ROOT}")
-	set(MPFR_ROOT_RELEASE "${MPFR_ROOT}")
+	my_find_include(MPFR_INCLUDE_DIR mpfr.h ${MPFR_ROOT})
+	my_find_lib(MPFR_LIBRARY mpfr ${MPFR_ROOT})
 endif()
 
-my_find_include(MPFR_INCLUDE_DIR mpfr.h ${MPFR_ROOT_DEBUG})
-
-my_find_lib(MPFR_LIBRARY_DEBUG mpfr ${MPFR_ROOT_DEBUG})
-my_find_lib(MPFR_LIBRARY_RELEASE mpfr ${MPFR_ROOT_RELEASE})
-
-set(MPFR_LIBRARY debug ${MPFR_LIBRARY_DEBUG} optimized ${MPFR_LIBRARY_RELEASE})
-
 mark_as_advanced(MPFR_INCLUDE_DIR MPFR_LIBRARY)
-mark_as_advanced(MPFR_LIBRARY_DEBUG MPFR_LIBRARY_RELEASE)
 
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(MPFR
 	REQUIRED_VARS
-		MPFR_INCLUDE_DIR
 		MPFR_LIBRARY
-		MPFR_LIBRARY_DEBUG
-		MPFR_LIBRARY_RELEASE
+		MPFR_INCLUDE_DIR
 )
